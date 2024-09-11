02d7b6d3c2838854c6ee7f852acb3b0fa2e89b85
Lodalesan <gangsterking002@bigmumbai123.site>
2024-09-11 12:17:39 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7538952567:AAHK4cajledU7MAxLQlzTR-WWVFaCQyhtzk")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Jatt")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6186321214")
+
+
+
+
+
+
+
+  
